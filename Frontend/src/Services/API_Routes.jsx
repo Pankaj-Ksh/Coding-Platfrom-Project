35a7b24d9ff283dea18d9bfd622fcbd904807e84
Pankaj-Ksh <pankajksh00@gmail.com>
2024-09-23 19:50:24 +0530

    updated API_Routes

--- Frontend/src/Services/API_Routes.jsx
+++ Frontend/src/Services/API_Routes.jsx
@@ -1,10 +1,9 @@
-const BASE_URL = "http://localhost:3000/api/v1";
+const BASE_URL = import.meta.env.VITE_BACKEND_BASE_URL;
 
 export const authEndPoints = {
     LOGIN_API : BASE_URL + "/auth/login",
     SIGNUP_API : BASE_URL + "/auth/signup"
 }
-
 
 export const projectsEndPoints = {
     CREATE_PROJECT_API : BASE_URL + "/project/createproject",
